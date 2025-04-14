b6fb1c6b9458edd189a16ecc937c75b87a48c9b8
hasmetdurak <hasmetdurak@gmail.com>
2025-04-14 23:53:19 +0300

    Create social_media.py

---
+++ social_media.py
@@ -0,0 +1,12 @@
+from facp import FACPSuperBridge
+
+resources = [
+    {"url": "https://api.twitter.com", "type": "social"}
+]
+
+bridge = FACPSuperBridge(resources=resources, credentials={"Authorization": "Bearer your-token"})
+mcp_app = bridge.create_mcp_server()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(mcp_app, host="0.0.0.0", port=8001)
